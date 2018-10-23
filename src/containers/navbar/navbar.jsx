f12a8770fecc6a61b8536dc2dae1073662523967
tushar0837 <tushar0837@gmail.com>
2018-10-23 21:25:22 +0530

    nav css dletion

--- src/containers/navbar/navbar.jsx
+++ src/containers/navbar/navbar.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-import './navbar.css'
 import { withRouter } from 'react-router-dom'
 import { auth } from '../../auth'
 import NavbarConponent from '../../components/navbar'
